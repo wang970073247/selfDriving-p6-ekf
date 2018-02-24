9fae94099477900185d28fc2e957edf7979b651b
wang <970073247@qq.com>
2018-02-24 23:35:33 +0800

    modify initialize and requirement to call ekf

---
+++ src/kalman_filter.cpp
@@ -0,0 +1,96 @@
+#include "kalman_filter.h"
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+// Please note that the Eigen library does not initialize 
+// VectorXd or MatrixXd objects with zeros upon creation.
+
+KalmanFilter::KalmanFilter() {}
+
+KalmanFilter::~KalmanFilter() {}
+
+void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
+                        MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
+  x_ = x_in;
+  P_ = P_in;
+  F_ = F_in;
+  H_ = H_in;
+  R_ = R_in;
+  Q_ = Q_in;
+}
+
+void KalmanFilter::Predict() {
+  /**
+  TODO:
+    * predict the state
+  */
+  x_ = F_ * x_;
+  P_ = F_ * P_ * F_.transpose() + Q_;
+}
+
+void KalmanFilter::Update(const VectorXd &z) {
+  /**
+  TODO:
+    * update the state by using Kalman Filter equations
+  */
+  VectorXd y = z - H_ * x_;
+    
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd K =  P_ * Ht * Si;
+    
+  // New state
+  unsigned int x_size = x_.size();
+  x_ = x_ + (K * y);
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
+}
+
+void KalmanFilter::UpdateEKF(const VectorXd &z) {
+  // Get predicted location in polar coords.
+  float px = x_(0);
+  float py = x_(1);
+  float vx = x_(2);
+  float vy = x_(3);
+    
+  // Check the division by 0.
+  float eps = 0.000001;
+  if (fabs(px) < eps && fabs(py) < eps)
+  {
+    return;
+  }
+
+    
+    
+  // Recalculate x object state to rho, theta, rho_dot coordinates
+  float rho = sqrtf(powf(px, 2) + powf(py, 2));
+  float theta = atan2(py, px);
+  float rho_dot = (px * vx + py * vy) / rho;
+    
+  VectorXd z_pred = VectorXd(3);   // h(x_)
+  z_pred << rho, theta, rho_dot;
+    
+  VectorXd y = z - z_pred;
+  if(y(1) > M_PI)
+  {
+    y(1) -= 2 * M_PI;
+  }
+  else if(y(1) <= -M_PI)
+  {
+    y(1) += 2*M_PI;
+  }
+    
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd K =  P_ * Ht * Si;
+    
+  // New state
+  int x_size = x_.size();
+  x_ = x_ + (K * y);
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
+}
+
---
+++ src/test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int m = 16;
+    cout << sqrt(m) << endl;
+    return 0;
+}
